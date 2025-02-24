822c367b7db0c44199c4bb889def95375d019eb7
alex <53851759+alxxjohn@users.noreply.github.com>
2025-02-24 14:16:08 -0500

    save

---
+++ foodshareapp_api/tests/test_basics.py
@@ -0,0 +1,45 @@
+import pytest
+import json
+import asyncio
+from httpx import AsyncClient
+import uuid
+from starlette.testclient import TestClient
+from starlette import status
+
+from foodshareapp_api.app.application import app
+
+
+client = TestClient(app)
+
+
+def test_openapi_json():
+    response = client.get("/api/openapi.json")
+    assert response.status_code == status.HTTP_200_OK, response.json
+
+
+def test_doc():
+    response = client.get('/api/docs')
+    assert response.status_code == status.HTTP_200_OK, response.text
+
+
+def test_redoc():
+    response = client.get('/api/redoc')
+    assert response.status_code == status.HTTP_200_OK, response.text
+
+
+def test_health():
+    response = client.get('/api/health')
+    assert response.status_code == status.HTTP_200_OK, response.text
+
+
+def test_echo():
+    message = "hello"
+    url = '/api/echo/'
+    response = client.post(
+        url,
+        json={
+            "message": message,
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["message"] == message
