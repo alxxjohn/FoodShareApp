6606928336be44f87392a48c89d2d42d898013a0
alex <53851759+alxxjohn@users.noreply.github.com>
2025-02-24 14:05:26 -0500

    init

---
+++ foodshareapp_api/app/api/router.py
@@ -0,0 +1,9 @@
+from fastapi.routing import APIRouter
+
+from foodshareapp.app.api.routes import (docs, echo, health)
+
+
+api_router = APIRouter()
+api_router.include_router(health.router)
+api_router.include_router(docs.router)
+api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
---
+++ foodshareapp_api/app/api/routes/health.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from foodshareapp_api.db.utils import redis_db
+router = APIRouter()
+
+
+@router.get("/health")
+def health_check() -> None:
+    """
+    Checks the health of a project.
+
+    It returns 200 if the project is healthy.
+    """
+
+
+@router.get("/data")
+def rediget(value: str) -> str:
+    redis_db.set(value)
+    return value
