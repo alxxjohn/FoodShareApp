9a5a6de09149cac10da5030e566e1233323af43f
alex <53851759+alxxjohn@users.noreply.github.com>
2025-02-24 14:55:47 -0500

    setup

--- foodshareapp/__main__.py
+++ foodshareapp/__main__.py
@@ -6,7 +6,7 @@ from foodshareapp.settings import settings
 def main() -> None:
     """Entrypoint of the application."""
     uvicorn.run(
-        "foodshareapp.app.application:get_app",
+        "foodshareapp.app.api.application:get_app",
         workers=settings.workers_count,
         host=settings.host,
         port=settings.port,
