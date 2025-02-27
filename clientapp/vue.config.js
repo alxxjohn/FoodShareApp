f2152c6fdbcee9d5cdcbbe237130e43344e87822
ine <inepark22@gmail.com>
2025-02-27 14:13:47 -0500

    modified docker-compose.yml to include client app, and create/modify to configure client app

--- clientapp/vue.config.js
+++ clientapp/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://api:8000", 
+        changeOrigin: true,
+        pathRewrite: { 
+          "^/api": "/api"  // Keep the /api in the request path
+        },
+      },
+    },
+  },
+});
+
