491bccc3ca6c574651ca9c2db8cd585a6a63f295
ine <inepark22@gmail.com>
2025-03-02 20:16:50 -0500

    modified docker files to have dev and prod a different server(vue_cli, nginx)

--- clientapp/vue.config.js
+++ clientapp/vue.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: '0.0.0.0',  // Ensure the server is accessible from outside the container
+    port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", 
+        target: "http://api:8000", 
         changeOrigin: true,
         pathRewrite: { 
           "^/api": "/api"  // Keep the /api in the request path
