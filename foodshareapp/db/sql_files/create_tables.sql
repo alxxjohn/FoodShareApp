a1024fa761afc8d3bed543b71d4fb56d84886010
alex <53851759+alxxjohn@users.noreply.github.com>
2025-03-07 16:06:11 -0500

    save

---
+++ foodshareapp/db/sql_files/create_tables.sql
@@ -0,0 +1,41 @@
+
+-- foodshare users table -- 
+
+CREATE TABLE IF NOT EXISTS foodshare_users (
+    uuid uuid NOT NULL,
+    email text NOT NULL,
+    username text NOT NULL,
+    firstname text NOT NULL,
+    lastname text NOT NULL,
+    salt text NOT NULL,
+    password text NOT NULL,
+    tos_accepted boolean NOT NULL DEFAULT true,
+    tos_accepted_date timestamp with time zone NOT NULL DEFAULT now(),
+    last_login timestamp with time zone,
+    bad_login_attempt timestamp with time zone,
+    bad_login_count integer,
+    account_locked boolean NOT NULL DEFAULT false,
+    account_verified boolean DEFAULT true,
+    account_verified_at timestamp,
+
+
+    PRIMARY KEY (uuid),
+    CONSTRAINT unique_email UNIQUE (email)
+
+);
+
+
+-- todo add business table -- 
+
+
+
+
+
+-- todo add donations table -- 
+
+
+
+
+-- todo add foodbanks table -- 
+
+
