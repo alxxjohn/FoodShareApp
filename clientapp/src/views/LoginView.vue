79b3ff4964f0d1e2d6903c70fd984c1bd54beb05
ine <inepark22@gmail.com>
2025-03-10 14:08:39 -0400

    add LoginView page

--- clientapp/src/views/LoginView.vue
+++ clientapp/src/views/LoginView.vue
@@ -1,21 +1,34 @@
 <template>
-  <form name="login-form" >
-    <div class="mb-3">
-      <label for="username">Username: </label>
-      <input id="username" type="text" />
+  <form name="login-form">
+    <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="username" placeholder="Username" v-model="login_info.username">
+    <label for="username">Username</label>
     </div>
-    <div class="mb-3">
-      <label for="password">Password: </label>
-      <input id="password" type="password" />
+    <div class="form-floating">
+      <input type="password" class="form-control" id="password" placeholder="Password" v-model="login_info.username">
+      <label for="password">Password</label>
     </div>
-    <button class="btn btn-primary" type="submit">
-      Login
-    </button>
-  </form>
+    <div class="d-flex gap-2">
+      <button class="btn btn-primary" type="submit">Sign In</button>
+      <button class="btn btn-primary" type="button">Register</button>
+    </div>
+  </form> 
 </template>
 
-<script>
-export default {
-name: 'LoginView',
-}
-</script>
+<!-- <button type="button" class="btn btn-secondary" onclick="alert('Hello!')">Click Me</button> 
+ I need to do it for the button Register-->
+
+
+ <script>
+ export default {
+   name: 'LoginView',
+   data(){
+     return{
+         login_info:{
+             username: "",
+             password: ""
+         }
+     }
+   }
+ }
+ </script>
