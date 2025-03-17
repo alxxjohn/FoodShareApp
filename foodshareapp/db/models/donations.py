fbe2cc07081eb34c38fc8e13ea3a984047f139a8
alex <53851759+alxxjohn@users.noreply.github.com>
2025-03-17 12:17:53 -0400

    save

---
+++ foodshareapp/db/models/donations.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from dataclasses import asdict
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel
+
+from foodshareapp.db.utils import db
+
+
+@dataclass
+class donationsModel:
+    pass
+
+
+@dataclass
+class donations:
+    pass
