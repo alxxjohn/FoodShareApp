d7ffd7a6bb46381e4c731107ced63d21ee357dc4
Dillon Kilgallon <dillonkilgallon@gmail.com>
2025-04-09 17:12:19 -0400

    User data retrieval via auth token is working now

--- clientapp/src/views/UserHome.vue
+++ clientapp/src/views/UserHome.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Hello {{ this.username }}</h1>
+    <h1>Hello {{ user_info.username }}</h1>
  </template>
   
   <script>
@@ -22,28 +22,24 @@ mounted() {
 methods: {
     async getUserData() {
         const token = localStorage.getItem('access_token');
-    const response = axios.get('http://localhost:8000/api/auth/user', {
+    const response = await axios.get('http://localhost:8000/api/auth/user', {
  headers: {
    'Accept': 'application/json',
    'Authorization': `Bearer ${token}`
  }})
 
- if (response && response.data) {
-            // Update user_info with the response data
-            this.user_info.username = response.data.username;
-            this.user_info.email = response.data.email;
-            this.user_info.uuid = response.data.uuid;
 
-            // Log the updated user_info after receiving the data
-            console.log('After API request:', this.user_info);
-          }
+  this.user_info.username = response.data.username;
+  this.user_info.email = response.data.email;
+  this.user_info.uuid = response.data.uuid;
 
+  console.log('After API request:', this.user_info);
 
 
 const str = JSON.stringify(token);
           console.log("Data = " + str);
- const str2 = JSON.stringify(response.username);
- console.log("Username = " + str2);
+ const str2 = JSON.stringify(response.data.username);
+ console.log("response = " + str2);
     }
 
  }
